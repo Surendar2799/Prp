aa3de8e60d660b578a8c4515d9d4d5debd91d47e
Surendar2799 <51944474+Surendar2799@users.noreply.github.com>
2019-07-01 15:12:23 +0530

    Add files via upload

---
+++ Wipro1.java
@@ -0,0 +1,18 @@
+package Wipro;
+import java.util.Scanner;
+public class Wipro1 {
+	public static void main(String[] args) {
+		Scanner n=new Scanner (System.in);
+		System.out.println("Enter the values");
+		int s=n.nextInt();
+		if(s>0)
+			System.out.println("Given " +s+ " is Positive");
+		else if(s<0)
+			System.out.println("Given " +s+ " is Negative");
+		else
+			System.out.println("Given " +s+ " is Zero");
+		n.close();
+
+	}
+
+}
