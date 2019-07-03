6ef5e19219d224410e5c9500c013afac229398c1
Surendar2799 <51944474+Surendar2799@users.noreply.github.com>
2019-07-03 15:06:10 +0530

    Add files via upload

---
+++ N_Find_5.java
@@ -0,0 +1,17 @@
+package Guvi;
+import java.util.*;
+public class N_Find_5 {
+	public static void main(String[] args) {
+		System.out.println("Enter the values");
+		Scanner a=new Scanner(System.in);
+		int n=a.nextInt();
+		int b=0;
+		for(int i=1;i<=5;i++)
+		{
+			
+			System.out.print(" "+n*i);
+		}
+
+	}
+
+}
