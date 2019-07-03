162bb1cdd9d7ef67de679622cbc76a0de0d8c9dd
Surendar2799 <51944474+Surendar2799@users.noreply.github.com>
2019-07-03 12:21:40 +0530

    Add files via upload

---
+++ Set14.java
@@ -0,0 +1,19 @@
+package Guvi;
+import java.util.Scanner;
+public class Set14 {
+	public static void main(String[] args) {
+		Scanner a=new Scanner (System.in);
+		System.out.println("Enter the range");
+		int m=a.nextInt();
+		int n=a.nextInt();
+		for (int i=m+1;i<n;i++)
+		{
+			if(i%2!=0) 
+				
+			{
+				System.out.print(i + " ");
+			}
+		}
+		a.close();
+	}
+}
