b4713de83493259c715bf6bb2a56c2bf0b8190aa
Surendar2799 <51944474+Surendar2799@users.noreply.github.com>
2019-07-03 12:02:57 +0530

    Add files via upload

---
+++ Paindrome.java
@@ -0,0 +1,22 @@
+package Guvi;
+import java.util.Scanner;
+public class Paindrome {
+	public static void main(String[] args) {
+		Scanner n=new Scanner(System.in);
+		System.out.println("Enter the values");
+		int s=n.nextInt();
+		int r=0,sum=0;
+		int temp=s;
+		while(s>0)
+		{
+			r=s%10;
+			sum=(sum*10)+r;
+			s=s/10;
+		}
+		if(temp==sum)
+		System.out.println("Yes");
+		else
+		System.out.println("No");
+		n.close();
+	}
+}
