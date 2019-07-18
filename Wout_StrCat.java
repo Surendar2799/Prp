36d699972e8c04eb492c65dac298263ab7de9dd7
Surendar2799 <51944474+Surendar2799@users.noreply.github.com>
2019-07-18 12:11:27 +0530

    Add files via upload

---
+++ Wout_StrCat.java
@@ -0,0 +1,16 @@
+package Guvi;
+
+import java.util.Scanner;
+
+public class Wout_StrCat {
+	public static void main(String[] args) {
+		System.out.println("Enter the two String");
+		Scanner s=new Scanner(System.in);
+		String a=s.nextLine();
+		String b=s.nextLine();
+		System.out.println(a+""+b);
+		
+
+	}
+
+}
