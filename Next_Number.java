ccbd83a17ade71490ed124e8c34e332b33d3edf6
Surendar2799 <51944474+Surendar2799@users.noreply.github.com>
2019-07-18 14:32:18 +0530

    Add files via upload

---
+++ Next_Number.java
@@ -0,0 +1,12 @@
+package Guvi;
+
+import java.util.Scanner;
+
+public class Next_Number {
+	public static void main(String[] args) {
+		Scanner n=new Scanner(System.in);
+		int a=n.nextInt();
+		System.out.println(a+1);
+		n.close();
+	}
+}
