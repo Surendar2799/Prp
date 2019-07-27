23c2969c423bf5a2a94a6f05dd21326d69417c00
Surendar2799 <51944474+Surendar2799@users.noreply.github.com>
2019-07-27 14:34:55 +0530

    Add files via upload

---
+++ Alphapets_Numbers.java
@@ -0,0 +1,22 @@
+package Guvi;
+
+import java.util.Scanner;
+
+public class Alphapets_Numbers {
+	public static void main(String[] args) {
+		Scanner n=new Scanner(System.in);
+		String a=n.next();
+		boolean b=false;
+		boolean c=false;
+		for(int i=0;i<a.length();i++)
+		{
+			if(b&&c)
+			{
+				break;
+			}
+			if(a.charAt(i))
+		}
+
+	}
+
+}
